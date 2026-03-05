310016221b28a6492ae1ca084a66236825e21c25
Vitalie Cucuietu <vcucuiet@f6r12s16.clusters.42paris.fr>
2026-03-05 18:03:38 +0100

    passed struct s_token to enum e_token

--- includes/lexer.h
+++ includes/lexer.h
@@ -6,7 +6,7 @@
 /*   By: vcucuiet <vcucuiet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 15:24:52 by vcucuiet          #+#    #+#             */
-/*   Updated: 2026/03/05 16:47:55 by vcucuiet         ###   ########.fr       */
+/*   Updated: 2026/03/05 18:02:15 by vcucuiet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,26 @@
 # define LEXER_H
 # include "libft.h"
 
-typedef struct s_token
+typedef enum e_token
 {
-	int	word;
-	int	in;
-	int	out;
-	int	here_doc;
-	int	append_out;
-	int	pipe;
-	int	is_and;
-	int	is_or;
-}			t_token_type;
+	TOKEN_WORD,
+	TOKEN_IN,
+	TOKEN_OUT,
+	TOKEN_HERE_DOC,
+	TOKEN_APPEND_OUT,
+	TOKEN_PIPE,
+	TOKEN_AND,
+	TOKEN_OR,
+}			t_token;
 
 typedef	struct s_lexer
 {
-	t_token_type	type;
-	char			*value;
-	t_lexer			*next;
+	t_token	type;
+	char	*value;
+	t_lexer	*next;
 }			t_lexer;
 
-t_lexer	*ft_lexnew(void *value);
+t_lexer	*ft_lexnew(char *value);
 int		ft_lexadd_back(t_lexer **lst, t_lexer *new);
 void	ft_lexclear(t_lexer **lst, void (*del)(void *));
 
