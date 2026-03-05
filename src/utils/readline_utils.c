b0f28131adb0a658034823749bd52ef23095c315
Mathieu Ville <mathieuv@MacBook-Pro-de-Mathieu.local>
2026-03-05 17:35:07 +0100

    add input management and global cleanup function

---
+++ src/utils/readline_utils.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   readline_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mville <mville@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/05 17:15:14 by mville            #+#    #+#             */
+/*   Updated: 2026/03/05 17:27:53 by mville           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	*get_input(t_shell *shell)
+{
+	char	*input;
+
+	input = readline("minishell > ");
+	if (input == NULL)
+	{
+		write(1, "exit\n", 5);
+		shell->run = 0;
+		return (NULL);
+	}
+	if (input[0] != '\0')
+		add_history(input);
+	return (input);
+}
+
+void	free_and_clean_history(t_shell *shell)
+{
+	rl_clear_history();
+	ft_free_tab(shell->env);
+}
