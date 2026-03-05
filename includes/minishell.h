cf4652d423263f668689afe96ca735e91a1bf947
Mathieu Ville <mathieuv@MacBook-Pro-de-Mathieu.local>
2026-03-05 17:32:32 +0100

    add prototypes

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,21 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mville <mville@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/05 16:37:12 by mville            #+#    #+#             */
+/*   Updated: 2026/03/05 17:27:25 by mville           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 typedef struct s_shell
 {
-	char    **env;
-	int     status_exit;
-	int     run;
-}   t_shell;
+	char	**env;
+	int		status_exit;
+	int		run;
+}	t_shell;
 
-void shell_init(t_shell *shell, char **envp);
+/* UTILS->INITIALIZER.C */
+int		shell_init(t_shell *shell, char **envp);
+
+/* UTILS->FREE.C */
+void	ft_free_tab(char **res);
+
+/* UTILS->READLINE_UTILS.C */
+char	*get_input(t_shell *shell);
+void	free_and_clean_history(t_shell *shell);
+
+/* UTILS->SIGNAL_UTILS.C */
+void	signal_init(void);
 
 #endif
