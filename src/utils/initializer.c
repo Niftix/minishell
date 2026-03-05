89857db5250532191a689f84e0948a3e44290b17
Mathieu Ville <mathieuv@MacBook-Pro-de-Mathieu.local>
2026-03-05 17:34:06 +0100

    improve with strdup

--- src/utils/initializer.c
+++ src/utils/initializer.c
@@ -6,7 +6,7 @@
 /*   By: mville <mville@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 16:36:14 by mville            #+#    #+#             */
-/*   Updated: 2026/03/05 16:58:13 by mville           ###   ########.fr       */
+/*   Updated: 2026/03/05 17:30:05 by mville           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,13 @@
 static int	duplicate_env(t_shell *shell, char **envp, int count)
 {
 	int	i;
-	int	j;
 
 	i = 0;
 	while (i < count)
 	{
-		j = 0;
-		while (envp[i][j])
-			j++;
-		shell->env[i] = malloc(sizeof(char) * (j + 1));
+		shell->env[i] = strdup(envp[i]);
 		if (!shell->env[i])
-			return (ft_free(shell->env), 1);
-		ft_strcpy(shell->env[i], envp[i]);
+			return (ft_free_tab(shell->env), 1);
 		i++;
 	}
 	shell->env[i] = NULL;
