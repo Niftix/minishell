eb0c854f15c254cb891f5def2b9502b4407bf4b3
Mathieu Ville <mathieuv@MacBook-Pro-de-Mathieu.local>
2026-03-05 17:35:48 +0100

    add signal init and signal handle

---
+++ src/utils/signal_utils.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signal_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mville <mville@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/05 17:23:26 by mville            #+#    #+#             */
+/*   Updated: 2026/03/05 17:25:07 by mville           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static void	sigint_handle(int sigint)
+{
+	(void)sigint;
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+void	signal_init(void)
+{
+	signal(SIGINT, sigint_handle);
+	signal(SIGQUIT, SIG_IGN);
+}
