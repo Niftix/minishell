c4334a7d3d60e7172a09d6c6d3b485e48a3008c4
Mathieu Ville <mathieuv@MacBook-Pro-de-Mathieu.local>
2026-03-05 16:44:49 +0100

    update main execution loop + (SIGINT/SIGQUIT)

--- main.c
+++ main.c
@@ -1,31 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mville <mville@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/05 16:35:00 by mville            #+#    #+#             */
+/*   Updated: 2026/03/05 16:42:52 by mville           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-void sigint_handle(int sigint)
+void	sigint_handle(int sigint)
 {
- (void)sigint;
- write(1, "\n", 1);
- rl_on_new_line();
- rl_replace_line("", 0);
- rl_redisplay();
+	(void)sigint;
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
 }
 
-int main(int ac, char **av, char **envp)
+int	main(int ac, char **av, char **envp)
 {
-  t_shell shell;
-  char *input;
+	t_shell	shell;
+	char	*input;
 
-  shell_init(&shell, &envp);
-  signal(SIGINT, sigint_handle);
-while (shell.run)
-{
- input = readline("minishell > ");
- if (input == NULL)
-  shell.run = 0;
- if (input[0] != '\0')
-   add_history(input);
-   // APPEL DU PARSING
-  free(input);
-}
-re_clear_history();
-return (0);
+	signal(SIGINT, sigint_handle);
+	signal(SIGQUIT, SIG_IGN);
+	if (shell_init(&shell, envp) == 1)
+		return (1);
+	while (shell.run)
+	{
+		input = readline("minishell > ");
+		if (input == NULL)
+		{
+			write(1, "exit\n", 5);
+			shell.run = 0;
+		}
+		else
+		{
+			if (input[0] != '\0')
+				add_history(input);
+		}
+    //PARSE + EXEC
+		free(input);
+	}
+	rl_clear_history();
+	return (0);
 }
