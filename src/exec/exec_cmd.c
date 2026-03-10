fd00b3cd32c064ea6028e7d53652b06120f03dea
Mathieu Ville <mathieuv@MacBook-Pro-de-Mathieu.local>
2026-03-10 12:25:22 +0100

    add exit message

--- src/exec/exec_cmd.c
+++ src/exec/exec_cmd.c
@@ -6,11 +6,28 @@
 /*   By: mville <mville@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 21:16:54 by mville            #+#    #+#             */
-/*   Updated: 2026/03/10 12:17:54 by mville           ###   ########.fr       */
+/*   Updated: 2026/03/10 12:24:50 by mville           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void execve_fail_exit(t_ast *ast, char *path)
+{
+	ft_putstr_fd("minishell :", 2);
+	ft_putstr_fd(ast->args_cmd[0], 2);
+	ft_putstr_fd(": permission denied\n", 2);
+	free(path);
+	exit(126);
+}
+
+static void path_null_exit(t_ast *ast)
+{
+	ft_putstr_fd("minishell :", 2);
+	ft_putstr_fd(ast->args_cmd[0], 2);
+	ft_putstr_fd(": command not found\n", 2);
+	exit(127);
+}
 
 static int gest_signal(void)
 {
@@ -36,10 +53,9 @@ int	exec_cmd(t_shell *shell, t_ast *ast)
 		gest_signal();
 		path = find_cmd_path(shell, ast);
 		if (!path)
-			exit(127);
+			path_null_exit(ast);
 		execve(path, ast->args_cmd, shell->env);
-		free(path);
-		exit(126);
+		execve_fail_exit(ast, path);
 	}
 	waitpid(pid, &status, 0);
 	return (WEXITSTATUS(status));
