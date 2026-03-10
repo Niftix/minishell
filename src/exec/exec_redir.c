6cdd4b1013d3b48245ea3f5985723a7a83df0aee
Mathieu Ville <mathieuv@MacBook-Pro-de-Mathieu.local>
2026-03-10 21:32:37 +0100

    add all redirection

---
+++ src/exec/exec_redir.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_redir.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mville <mville@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/10 10:52:17 by mville            #+#    #+#             */
+/*   Updated: 2026/03/10 20:12:09 by mville           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	redir_in(char *target)
+{
+	int	fd;
+
+	fd = open(target, O_RDONLY);
+	if (fd == -1)
+	{
+		ft_putstr_fd("minishell: ", 2);
+		ft_putstr_fd(target, 2);
+		ft_putstr_fd(": No such file or directory\n", 2);
+		return (1);
+	}
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+	return (0);
+}
+
+static int	redir_out(char *target, int append)
+{
+	int	fd;
+	int	flags;
+
+	if (append)
+		flags = O_WRONLY | O_CREAT | O_APPEND;
+	else
+		flags = O_WRONLY | O_CREAT | O_TRUNC;
+	fd = open(target, flags, 0644);
+	if (fd == -1)
+	{
+		ft_putstr_fd("minishell: ", 2);
+		ft_putstr_fd(target, 2);
+		ft_putstr_fd(": Permission denied\n", 2);
+		return (1);
+	}
+	dup2(fd, STDOUT_FILENO);
+	close(fd);
+	return (0);
+}
+
+static int	redir_heredoc(char *delimiter)
+{
+	int		fd[2];
+	char	*line;
+
+	if (pipe(fd) == -1)
+		return (1);
+	while (1)
+	{
+		line = readline("> ");
+		if (!line || ft_strcmp(line, delimiter) == 0)
+		{
+			free(line);
+			break ;
+		}
+		ft_putstr_fd(line, fd[1]);
+		ft_putstr_fd("\n", fd[1]);
+		free(line);
+	}
+	close(fd[1]);
+	dup2(fd[0], STDIN_FILENO);
+	close(fd[0]);
+	return (0);
+}
+
+int	all_redirects(t_redirect *redirects)
+{
+	while (redirects)
+	{
+		if (redirects->type == REDIR_IN)
+		{
+			if (redir_in(redirects->target))
+				return (1);
+		}
+		else if (redirects->type == REDIR_OUT)
+		{
+			if (redir_out(redirects->target, 0))
+				return (1);
+		}
+		else if (redirects->type == REDIR_APPEND)
+		{
+			if (redir_out(redirects->target, 1))
+				return (1);
+		}
+		else if (redirects->type == REDIR_HEREDOC)
+		{
+			if (redir_heredoc(redirects->target))
+				return (1);
+		}
+		redirects = redirects->next;
+	}
+	return (0);
+}
