748c738bd5844c9abd29f629b130737aac1cf8b8
Mathieu Ville <mathieuv@MacBook-Pro-de-Mathieu.local>
2026-03-10 21:36:15 +0100

    add cd utils : find home + update env

---
+++ src/builtins/builtins_cd_utils.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins_cd_utils.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mville <mville@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/10 18:39:45 by mville            #+#    #+#             */
+/*   Updated: 2026/03/10 19:33:52 by mville           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	*find_home(t_shell *shell)
+{
+	int	i;
+
+	i = 0;
+	while (shell->env[i])
+	{
+		if (ft_strncmp(shell->env[i], "HOME=", 5) == 0)
+			return (shell->env[i] + 5);
+		i++;
+	}
+	return (NULL);
+}
+
+int	update_env(t_shell *shell, char *old_pwd, char *new_pwd)
+{
+	int	i;
+
+	i = 0;
+	while (shell->env[i])
+	{
+		if (ft_strncmp(shell->env[i], "PWD=", 4) == 0)
+		{
+			free(shell->env[i]);
+			shell->env[i] = ft_strjoin("PWD=", new_pwd);
+		}
+		else if (ft_strncmp(shell->env[i], "OLDPWD=", 7) == 0)
+		{
+			free(shell->env[i]);
+			shell->env[i] = ft_strjoin("OLDPWD=", old_pwd);
+		}
+		i++;
+	}
+	return (0);
+}
