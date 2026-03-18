cd7e48c12e36a6aff5efff12931a6290be395bee
Mathieu Ville <mathieuv@MacBook-Pro-de-Mathieu.local>
2026-03-18 10:14:22 +0100

    AST construction from tokens utils

---
+++ src/parsing/ast_parsing_util.c
@@ -0,0 +1,28 @@
+#include "parser.h"
+
+int	redirect_checker(t_token type)
+{
+	return (type == TOKEN_IN || type == TOKEN_OUT
+		|| type == TOKEN_APPEND_OUT || type == TOKEN_HERE_DOC);
+}
+
+int	cmd_token_checker(t_token type)
+{
+	return (type == TOKEN_WORD || redirect_checker(type));
+}
+
+int	count_words(t_lexer *current)
+{
+	int	count;
+
+	count = 0;
+	while (cmd_token_checker(current->type))
+	{
+		if (current->type == TOKEN_WORD)
+			count++;
+		else
+			current = current->next;
+		current = current->next;
+	}
+	return (count);
+}
