e3362d636520d41d1453b533833c20e84d73be54
Vitalie Cucuietu <vcucuiet@f6r13s16.clusters.42paris.fr>
2026-03-31 17:59:29 +0200

    need just to merge var in str

--- src/expand/expand.c
+++ src/expand/expand.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   expand.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vcucuiet <vita@student.42.fr>              +#+  +:+       +#+        */
+/*   By: vcucuiet <vcucuiet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/28 13:57:54 by vcucuiet          #+#    #+#             */
-/*   Updated: 2026/03/30 23:50:34 by vcucuiet         ###   ########.fr       */
+/*   Updated: 2026/03/31 14:25:48 by vcucuiet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,6 @@ static int	exp_is_var_sep(char c)
 			|| c == ' ' || c == '\t' || c == '\n')
 		return (1); //expand
 	return (0); //continue
-}
-
-static int	exp_cnt_var(char *str)
-{
-    int i = 0;
-    while (*str)
-    {
-        if (*str == '$')
-            i++;
-        str++;
-    }
-    return (i);
 }
 
 static char	*exp_grab_var(char *str)
@@ -50,38 +38,77 @@ static char	*exp_grab_var(char *str)
 	var[var_len] = '\0';
 	while (idx < var_len)
 	{
-		var[idx] = str[idx];
+		var[idx] = *str;
 		idx++;
 	}
 	return (var);
 }
 
-static size_t	exp_merge_var_to_str(char *str, char *var, size_t idx)
+static char	*exp_chr_var_in_env(char **env, char *var)
+{
+	size_t	i;
+	size_t	j;
+	size_t	var_len;
+
+	var_len = ft_strlen(var);
+	i = -1;
+	while (env[++i])
+	{
+		j = 0;
+		while (env[i][j] && var[j] && env[i][j] == var[j])
+			j++;
+		if (j == var_len && env[i][j] == '=')
+			return (ft_strdup(env[i] + (j + 1)));
+	}
+	return (ft_strdup(""));
+}
+
+static char	exp_exchange_var(char *str, char var, char *new_var, size_t *idx)
+{
+	size_t	old_v_len;
+	size_t	new_v_len;
+	char	*remain;
+
+	old_v_len = ft_strlen(var);
+	new_v_len = ft_strlen(new_var);
+	remain = ft_strdup(str + (*idx + old_v_len + 1));
+	if (!remain)
+		return (free(str), NULL);
+	while (new_var)
+	
+}
+
+static size_t	exp_merge_var_to_str(char *str, char **env, char *var, size_t idx)
 {
 	size_t	new_idx;
+	size_t	old_size;
+	size_t	new_size;
+	char	*new_var;
 
 	new_idx = idx;
-	//NEXT STEP
+	new_var = exp_chr_var_in_env(env, var);
+	if (!new_var)
+		return (NULL);//malloc error
+	old_size = ft_strlen(str);
+	new_size = old_size + ft_strlen(new_var) - ft_strlen(var) + 1;
+	if (new_size > old_size)
+	{
+		str = ft_realloc(str, old_size, new_size);
+		if (!str)
+			return (NULL);//malloc error
+	}
+	str = exp_exchange_var(str, var, new_var, &new_idx);
+	if (!str)
+		return (NULL);//malloc error
 	free(var);
 	var = NULL;
+	if (new_size < old_size)
+		str = ft_realloc(str, old_size, new_size);
 	return (new_idx);
 }
 
-char	*expand(char *str, char	**env)
+static char	*exp_chr(char *str, char **env, int idx, char quote, char *var)
 {
-	int		nb_var;
-	char	quote;
-	char	*var;
-	size_t	idx;
-
-	if (!str)
-		return (NULL);
-	nb_var = exp_cnt_var(str);
-	if  (nb_var == 0)
-		return (str);
-	idx = 0;
-	quote = 'x';
-	var = NULL;
 	while (str[idx])
 	{
 		if (quote == 'x' && (str[idx] == '\'' || str[idx] == '\"'))
@@ -95,8 +122,27 @@ char	*expand(char *str, char	**env)
 				return (NULL); //malloc error
 		}
 		if (var)
-			idx = exp_merge_var_to_str(str, var, idx);
+		{
+			idx = exp_merge_var_to_str(str, env, var, idx);
+			if (!var)
+				return (NULL); // malloc error
+		}
 		idx++;
 	}
+}
+char	**expand(char *str, char	**env)
+{
+	char	quote;
+	char	*var;
+	size_t	idx;
+
+	if (!str)
+		return (NULL);
+	idx = 0;
+	quote = 'x';
+	var = NULL;
+	str = exp_chr(str, env, idx, quote, var);
+	if (!str)
+		return (NULL); // malloc error
 	return (str);
 }
