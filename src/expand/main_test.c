a0dc35cbf5d13f1ed46984ecb161e2af2efac4cd
Vita <vitalie.cucuiet@gmail.com>
2026-04-06 17:54:01 +0200

    main_test for expand

--- src/expand/main_test.c
+++ src/expand/main_test.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main_test.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vcucuiet <vcucuiet@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vcucuiet <vita@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 14:19:18 by vcucuiet          #+#    #+#             */
-/*   Updated: 2026/04/02 16:00:08 by vcucuiet         ###   ########.fr       */
+/*   Updated: 2026/04/06 17:48:05 by vcucuiet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char	**expand(char *str, char **env);
+char	**expand(char *str, char **env, int exit_status);
 
 int	main(int ac, char **av, char **env)
 {
-	char	*str = ft_strdup("$USER");
+	char	*str = ft_strdup("salut $USER \"tu es la:&PWD\"  \'last_exit:$?\'");
 	char	**exp = NULL;
 
 	((void)ac, (void)av);
-	exp = expand(str, env);
-	printf("%s\n", exp[0]);
-	printf("%s\n", exp[1]);
-	if (exp)
-	{
-		free(exp[0]);
-		free(exp);
-	}
+	exp = expand(str, env, 0);
+	for (int i = 0; exp[i]; i++)
+		printf("%s\n", exp[i]);
+	ft_free2c(exp);
+	return (0);
 }
