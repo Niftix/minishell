da1657442233a3c8d4d988ccc4e0b4b30c9ed67c
mville <mville@student.42.fr>
2026-04-07 13:02:12 +0200

    add redirect validation

---
+++ src/parser/parser_redirect.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_redirect.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mville <mville@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/07 12:31:30 by mville            #+#    #+#             */
+/*   Updated: 2026/04/07 12:31:41 by mville           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parser.h"
+
+int	redirect_checker(t_token type)
+{
+	return (type == TOKEN_IN || type == TOKEN_OUT
+		|| type == TOKEN_APPEND_OUT || type == TOKEN_HERE_DOC);
+}
+
+int	cmd_token_checker(t_token type)
+{
+	return (type == TOKEN_WORD || redirect_checker(type));
+}
+
+t_redirect_type	token_to_redir(t_token type)
+{
+	if (type == TOKEN_IN)
+		return (REDIR_IN);
+	if (type == TOKEN_OUT)
+		return (REDIR_OUT);
+	if (type == TOKEN_APPEND_OUT)
+		return (REDIR_APPEND);
+	return (REDIR_HEREDOC);
+}
+
+int	parse_one_redirect(t_lexer **cur, t_redirect **list)
+{
+	t_redirect_type	rtype;
+	t_redirect		*redir;
+
+	rtype = token_to_redir((*cur)->type);
+	*cur = (*cur)->next;
+	if (!*cur || (*cur)->type != TOKEN_WORD)
+		return (1);
+	char	*target;
+
+	target = remove_quote((*cur)->value);
+	redir = redirect_new(rtype, target);
+	free(target);
+	if (!redir)
+		return (1);
+	redirect_add_end(list, redir);
+	*cur = (*cur)->next;
+	return (0);
+}
