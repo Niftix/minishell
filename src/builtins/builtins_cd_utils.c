af6c3732b0cb0d15bbdaaadcd558cce66a9ddfc3
mville <mville@student.42.fr>
2026-04-07 13:12:44 +0200

    add is_valid

--- src/builtins/builtins_cd_utils.c
+++ src/builtins/builtins_cd_utils.c
@@ -12,6 +12,22 @@
 
 #include "minishell.h"
 
+int	is_valid_id(char *s)
+{
+	int	i;
+
+	if (!s || (!ft_isalpha(s[0]) && s[0] != '_'))
+		return (0);
+	i = 1;
+	while (s[i] && s[i] != '=')
+	{
+		if (!ft_isalnum(s[i]) && s[i] != '_')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 char	*find_home(t_shell *shell)
 {
 	int	i;
