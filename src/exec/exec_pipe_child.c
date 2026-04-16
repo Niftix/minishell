5830ce7d528cc44f9049f06d39f5087ce9c80fb9
Mathieu V. <mathdigitale@gmail.com>
2026-04-16 16:04:15 +0200

    Fix conditional check for args_cmd in exec_pipe_child

--- src/exec/exec_pipe_child.c
+++ src/exec/exec_pipe_child.c
@@ -55,8 +55,12 @@ static int	exec_pipe_command(t_shell *shell, t_ast *ast)
 	char	*path;
 	char	**exec_env;
 
-	if (!ast->args_cmd || !ast->args_cmd[0] || !ast->args_cmd[0][0])
-		return (0);
+if (!ast->args_cmd || !ast->args_cmd[0] || !ast->args_cmd[0][0])
+{
+	if (ast->redirects && all_redirects(ast->redirects))
+		return (1);
+	return (0);
+}
 	if (ast->redirects && all_redirects(ast->redirects))
 		return (1);
 	if (check_builtins(ast->args_cmd[0]))
