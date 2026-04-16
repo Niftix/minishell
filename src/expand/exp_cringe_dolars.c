96c23201765872b5d88c5015d84a27e774de0848
Vita <vitalie.cucuiet@gmail.com>
2026-04-16 17:37:52 +0200

    add the missing else for incrementation

--- src/expand/exp_cringe_dolars.c
+++ src/expand/exp_cringe_dolars.c
@@ -6,7 +6,7 @@
 /*   By: vcucuiet <vita@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/10 16:59:18 by vcucuiet          #+#    #+#             */
-/*   Updated: 2026/04/16 15:56:23 by vcucuiet         ###   ########.fr       */
+/*   Updated: 2026/04/16 17:33:37 by vcucuiet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,8 @@ char	*exp_cringe_dolars(char *str)
 		if ((str[i] == '$' && quote == 'x')
 			&& ((str[i + 1] == '"' || str[i + 1] == '\'') && quote == 'x'))
 			str = exp_rm_cringe_dolars(str, i);
-		i++;
+		else
+			i++;
 	}
 	return (str);
 }
