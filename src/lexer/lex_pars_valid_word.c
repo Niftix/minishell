140e9e50a6ec0653795237e2bf9994c4c699e570
vcucuiet <vcucuiet@f6r13s16.paris.42.school>
2026-04-29 12:21:30 +0200

    add '{}' verification for variable

--- src/lexer/lex_pars_valid_word.c
+++ src/lexer/lex_pars_valid_word.c
@@ -6,34 +6,18 @@
 /*   By: vcucuiet <vcucuiet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 16:41:08 by vcucuiet          #+#    #+#             */
-/*   Updated: 2026/04/27 18:03:53 by vcucuiet         ###   ########.fr       */
+/*   Updated: 2026/04/29 11:21:26 by vcucuiet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
 
-static int	lex_put_brace_error()
+static int	lex_put_brace_error(char *str)
 {
-	
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(str, 2);
+	ft_putstr_fd(": bad substitution\n", 2);
 	return (3);
-}
-
-static char	lex_get_quote_state(char *str, size_t idx)
-{
-	size_t	i;
-	char	quote;
-
-	i = 0;
-	quote = 'x';
-	while (i < idx)
-	{
-		if (quote == 'x' && (str[i] == 39 || str[i] == 34))
-			quote = str[i];
-		else if (str[i] == quote)
-			quote = 'x';
-		i++;
-	}
-	return (quote);
 }
 
 static int	lex_verif_brace(char *str)
@@ -43,42 +27,32 @@ static int	lex_verif_brace(char *str)
 	int		tracer;
 
 	i = 0;
-	if (str[0] == '{')
+	if (str[0] == '}')
+		return (3);
+	tracer = 1;
+	brace = 1;
+	while (str[i] && brace && tracer &&
+			(ft_isalnum(str[i]) || str[i] == '{' || str[i] == '}' || str[i] == '_'))
 	{
-		tracer = 1;
-		brace = 1;
-		while (str[i] && brace && tracer)
-		{
-			if (str[i] == '{')
-				brace++;
-			if (str[i] == '{' && brace)
-				tracer++;
-			if (str[i] == '{')
-				brace--;
-			i++;
-		}
-		if (str[i] != '}')
-			return (2);
-		if (tracer != 1)
-			return (lex_put_brace_error());
+		if (str[i] == '{')
+			brace++;
+		if (str[i] == '{' && tracer)
+			tracer--;
+		if (str[i] == '}')
+			brace--;
+		i++;
 	}
+	if (tracer != 1)
+		return (3);
+	if (brace != 0)
+		return (2);
 	return (0);
 }
 
-static int	lex_varif_valid_variable(char *str)
+static int	lex_varif_valid_variable(char *str, int i)
 {
-	size_t	i;
-	int		check;
-
-	i = -1;
-	check = 0;
-	while (str[++i])
-	{
-		if (str[i] == '$' && lex_get_quote_state(str, i) != 39)
-		 	check = lex_verif_brace(str + i);
-		if (check)
-			return (check);
-	}
+	if (str[i + 1] == '{')
+		 return (lex_verif_brace(str + i + 2));
 	return (0);
 }
 
@@ -86,17 +60,23 @@ int	lex_pars_valid_word(char *str)
 {
 	size_t	i;
 	char	quote;
+	int		check;
 
 	quote = 'x';
 	i = -1;
+	check = 0;
 	while (str[++i])
 	{
 		if (quote == 'x' && (str[i] == 34 || str[i] == 39))
 			quote = str[i];
 		else if (str[i] == quote)
 			quote = 'x';
+		if (str[i] == '$' && quote != 39 && !check)
+			check = lex_varif_valid_variable(str, i);
 	}
 	if (quote != 'x')
 		return (3);
-	return (lex_varif_valid_variable(str));
+	if (check == 3)
+		lex_put_brace_error(str);
+	return (check);
 }
