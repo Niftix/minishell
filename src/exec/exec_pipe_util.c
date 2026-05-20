8bd3255a997949eb7ab3acde5d8020fdf5cc964b
Mathieu V. <mathdigitale@gmail.com>
2026-05-20 11:19:55 +0200

    Create exec_pipe_util.c

---
+++ src/exec/exec_pipe_util.c
@@ -0,0 +1,9 @@
+#include "minishell.h"
+
+int	pipe_empty_cmd(t_shell *shell, t_ast *ast)
+{
+	if (ast->redirects && all_redirects(ast->redirects))
+		return (1);
+	ft_putstr_fd("minishell: : command not found\n", 2);
+	return (127);
+}
