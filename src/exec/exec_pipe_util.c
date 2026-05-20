69f551f15c8230b1093471f3ad1a5fa06b910863
Mathieu V. <mathdigitale@gmail.com>
2026-05-20 11:35:04 +0200

    Add pipe_no_cmd function to check command redirects

--- src/exec/exec_pipe_util.c
+++ src/exec/exec_pipe_util.c
@@ -1,5 +1,12 @@
 #include "minishell.h"
 
+int	pipe_no_cmd(t_ast *ast)
+{
+	if (ast->redirects && all_redirects(ast->redirects))
+		return (1);
+	return (0);
+}
+
 int	pipe_empty_cmd(t_shell *shell, t_ast *ast)
 {
 	if (ast->redirects && all_redirects(ast->redirects))
