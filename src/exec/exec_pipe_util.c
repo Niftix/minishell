b03eabfbb988a1e67a2a29a95ef7454ceab7456b
Mathieu V. <mathdigitale@gmail.com>
2026-05-20 11:47:51 +0200

    Add no_exec_path_checker function for command validation

--- src/exec/exec_pipe_util.c
+++ src/exec/exec_pipe_util.c
@@ -14,3 +14,13 @@ int	pipe_empty_cmd(t_ast *ast)
 	ft_putstr_fd("minishell: : command not found\n", 2);
 	return (127);
 }
+
+int	no_exec_path_checker(char **no_exec, char *tmp)
+{
+	if (*no_exec || access(tmp, F_OK) != 0 || check_if_directory(tmp))
+		return (0);
+	*no_exec = ft_strdup(tmp);
+	if (!*no_exec)
+		return (1);
+	return (0);
+}
